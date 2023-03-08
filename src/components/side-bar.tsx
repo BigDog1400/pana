ac3bbe06327f242ed9f6480f51a5a41ed987ed62
BigDog1400 <const.isaac@gmail.com>
2023-03-08 01:50:19 -0400

    feat: add logout button

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -6,6 +6,9 @@ import { IoWalletOutline, IoWallet } from 'solid-icons/io';
 import { CgArrowsExchange } from 'solid-icons/cg';
 import { CgCalculator } from 'solid-icons/cg';
 import { BsWallet } from 'solid-icons/bs';
+import { Button } from '~/modules/ui/components/button';
+import { createServerAction$ } from 'solid-start/server';
+import { logout } from '~/db/session';
 export interface IconProps extends JSX.SvgSVGAttributes<SVGSVGElement> {
   size?: string | number;
   color?: string;
@@ -14,39 +17,49 @@ export interface IconProps extends JSX.SvgSVGAttributes<SVGSVGElement> {
 }
 
 export default function SideBar() {
+  const [, { Form }] = createServerAction$((f: FormData, { request }) => logout(request));
+
   return (
     // :class="sideBarOpen ? '' : 'hidden'" id="main-nav"
-    <div class="w-1/2 md:w-1/3 lg:w-64 fixed md:top-0 md:left-0 h-screen lg:block bg-white border-r z-30 hidden">
+    <div class="fixed z-30 hidden h-screen w-1/2 border-r bg-white md:top-0 md:left-0 md:w-1/3 lg:block lg:w-64">
       {/* <div class="w-full h-20 flex px-4 items-center mb-5">
         <p class="font-semibold text-3xl text-blue-400 pl-4">😶‍🌫️</p>
       </div> */}
-
-      <div class="mb-4 mt-5 px-4 space-y-1 text-gray-700">
-        {/* <p class="pl-4 text-sm font-semibold mb-1">Menu</p> */}
-        <A
-          href="/app/budgets"
-          class="w-full flex items-center h-10 pl-4 hover:bg-gray-200 rounded-sm cursor-pointer"
-          activeClass="bg-gray-200"
-        >
-          <CgCalculator class="h-6 w-6 mr-4" />
-          <span class="text-gray-700">Presupuesto</span>
-        </A>
-        <A
-          href="/app/transactions"
-          class="w-full flex items-center h-10 pl-4 hover:bg-gray-200 rounded-sm cursor-pointer"
-          activeClass="bg-gray-200"
-        >
-          <CgArrowsExchange class="h-6 w-6 mr-4" />
-          <span class="text-gray-700">Transactions</span>
-        </A>
-        <A
-          href="/app/wallets"
-          class="w-full flex items-center h-10 pl-4 hover:bg-gray-200 rounded-sm cursor-pointer"
-          activeClass="bg-gray-200"
-        >
-          <BsWallet class="h-6 w-6 mr-4" />
-          <span class="text-gray-700">Accounts</span>
-        </A>
+      <div class="flex h-full flex-col">
+        <div class="mb-4 mt-5 space-y-1 px-4 text-gray-700">
+          {/* <p class="pl-4 text-sm font-semibold mb-1">Menu</p> */}
+          <A
+            href="/app/budgets"
+            class="flex h-10 w-full cursor-pointer items-center rounded-sm pl-4 hover:bg-gray-200"
+            activeClass="bg-gray-200"
+          >
+            <CgCalculator class="mr-4 h-6 w-6" />
+            <span class="text-gray-700">Presupuesto</span>
+          </A>
+          <A
+            href="/app/transactions"
+            class="flex h-10 w-full cursor-pointer items-center rounded-sm pl-4 hover:bg-gray-200"
+            activeClass="bg-gray-200"
+          >
+            <CgArrowsExchange class="mr-4 h-6 w-6" />
+            <span class="text-gray-700">Transactions</span>
+          </A>
+          <A
+            href="/app/wallets"
+            class="flex h-10 w-full cursor-pointer items-center rounded-sm pl-4 hover:bg-gray-200"
+            activeClass="bg-gray-200"
+          >
+            <BsWallet class="mr-4 h-6 w-6" />
+            <span class="text-gray-700">Accounts</span>
+          </A>
+        </div>
+        <div class="mt-auto space-y-1 px-4 text-gray-700">
+          <Form>
+            <Button name="logout" type="submit" variant={'secondary'} width="full" fw={'semibold'}>
+              Logout
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
